408eed5d00bd9585d9b5a6fffba78eeb567c369f
buschbapti <baptiste.busch@inria.fr>
2017-02-23 17:17:36 +0100

    Add the download of linux executable

--- get_results_data.sh
+++ get_results_data.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
+mkdir -p results
+if [ ! -d "results/user_study_dataset" ]; then
+	cd results
+	echo "Downloading result files"
+	wget https://zenodo.org/record/321599/files/user_study_dataset.zip
+	unzip user_study_dataset.zip
+	rm user_study_dataset.zip
+	cd ..
+fi
 
-echo "Creating results folder"
-mkdir -p results && cd results
-echo "Downloading result files"
-wget https://zenodo.org/record/321599/files/user_study_dataset.zip
-unzip user_study_dataset.zip
-rm -rf user_study_dataset.zip
+
+mkdir -p bin
+if [ ! -d "bin/ReplayInterfaceLinux" ]; then
+	cd bin
+	echo "Downloading graphical interface binary"
+	wget https://github.com/3rdHand-project/PosturalOptimizationDataReader/releases/download/v1.0/ReplayInterfaceLinux.tar.gz
+	tar -xvzf ReplayInterfaceLinux.tar.gz
+	rm ReplayInterfaceLinux.tar.gz
+	cd ..
+fi
